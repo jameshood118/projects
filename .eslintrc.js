5b983810327a04df54fe28997e26e47a5e3ecc9c
James Hood <jhood@vatcinc.com>
2020-07-05 12:45:46 -0400

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,28 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  'extends': [
+    'plugin:vue/essential', '@vue/prettier'
+  ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? ['error', { allow: ['warn', 'error'] }]
+        : ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'space-before-function-paren': ['error', 'never'],
+    'no-unused-vars': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        tabWidth: 2,
+        singleQuote: true,
+        semi: false
+      }
+    ]
+  },
+}
