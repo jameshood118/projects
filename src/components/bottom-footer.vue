9a67a81422f75f4391fba75af0e718decc7469c3
James Hood <jhood@vatcinc.com>
2020-07-06 17:30:50 -0400

    update res link

--- src/components/bottom-footer.vue
+++ src/components/bottom-footer.vue
@@ -5,7 +5,8 @@
         2020 <a href="mailto:jameshood118@gmail.com">James Hood</a>,
         <a
           href="https://docs.google.com/document/d/181xOFDRX5Db15ah7LorR8M1lWT3ql4mBmpFdo89sgDE/edit?usp=sharing"
-          >Resume</a
+          targt="_blank"
+          >resume</a
         >
       </p>
     </div>
