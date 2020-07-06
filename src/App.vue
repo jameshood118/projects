e2dfb1f931913f375ca32cb06c826129fcbd6c64
James Hood <jhood@vatcinc.com>
2020-07-06 12:02:36 -0400

    add footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
     <Navigation />
     <div class="container is-fluid">
       <router-view />
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            2020 <a href="mailto:jameshood118@gmail.com">James Hood</a>,
+            <a
+              href="https://docs.google.com/document/d/1e-krbKFX3JcDGjjka1TLR94q-zRZLJY3jJlOcOTrVXQ/edit#heading=h.gjdgxs"
+              >Resume</a
+            >
+          </p>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
