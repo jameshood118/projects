f28a7cf5e67cd2e246bf912be7c579fe04a95808
James Hood <jhood@vatcinc.com>
2020-07-06 16:35:23 -0400

    move footer to footer component

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,19 @@
     <Navigation />
     <div class="container is-fluid">
       <router-view />
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>
-            2020 <a href="mailto:jameshood118@gmail.com">James Hood</a>,
-            <a
-              href="https://docs.google.com/document/d/1e-krbKFX3JcDGjjka1TLR94q-zRZLJY3jJlOcOTrVXQ/edit#heading=h.gjdgxs"
-              >Resume</a
-            >
-          </p>
-        </div>
-      </footer>
+      <BottomFooter />
     </div>
   </div>
 </template>
 
 <script>
 import Navigation from './components/navigation.vue'
+import BottomFooter from './components/bottom-footer.vue'
 export default {
   name: 'App',
   components: {
-    Navigation
+    Navigation,
+    BottomFooter
   }
 }
 </script>
---
+++ src/components/bottom-footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        2020 <a href="mailto:jameshood118@gmail.com">James Hood</a>,
+        <a
+          href="https://docs.google.com/document/d/181xOFDRX5Db15ah7LorR8M1lWT3ql4mBmpFdo89sgDE/edit?usp=sharing"
+          >Resume</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'bottom-footer'
+}
+</script>
+
+<style lang="scss" scoped></style>
