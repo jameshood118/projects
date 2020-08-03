63ec72e176b0b0647ffb554066dc19f9d6e8a325
James Hood <jhood@vatcinc.com>
2020-08-03 10:32:25 -0400

    tweak from macbook

--- src/components/accordion.vue
+++ src/components/accordion.vue
@@ -24,29 +24,29 @@
 
 <script>
 export default {
-  name: 'accordion',
-  data() {
-    return {
-      show: false
-    }
-  },
-  methods: {
-    toggle() {
-      this.show = !this.show
-    },
-    beforeEnter(el) {
-      el.style.height = '0'
-    },
-    enter(el) {
-      el.style.height = el.scrollHeight + 'px'
-    },
-    beforeLeave(el) {
-      el.style.height = el.scrollHeight + 'px'
-    },
-    leave(el) {
-      el.style.height = '0'
-    }
-  }
+	name: 'accordion',
+	data() {
+		return {
+			show: false
+		}
+	},
+	methods: {
+		toggle() {
+			this.show = !this.show
+		},
+		beforeEnter(el) {
+			el.style.height = '0'
+		},
+		enter(el) {
+			el.style.height = el.scrollHeight + 'px'
+		},
+		beforeLeave(el) {
+			el.style.height = el.scrollHeight + 'px'
+		},
+		leave(el) {
+			el.style.height = '0'
+		}
+	}
 }
 </script>
 
