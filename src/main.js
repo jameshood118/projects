5402c6024de6bf1142dace132d7d530ba759db64
James Hood <jameshood118@gmail.com>
2020-08-16 14:27:59 -0700

    add router.beforeEach for meta titles from router

--- src/main.js
+++ src/main.js
@@ -33,6 +33,38 @@ Vue.component('accordion', accordion)
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.title)
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.metaTags)
+
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title
+  Array.from(
+    document.querySelectorAll('[data-vue-router-controlled]')
+  ).map(el => el.parentNode.removeChild(el))
+  if (!nearestWithMeta) return next()
+  nearestWithMeta.meta.metaTags
+    .map(tagDef => {
+      const tag = document.createElement('meta')
+
+      Object.keys(tagDef).forEach(key => {
+        tag.setAttribute(key, tagDef[key])
+      })
+
+      tag.setAttribute('data-vue-router-controlled', '')
+
+      return tag
+    })
+    .forEach(tag => document.head.appendChild(tag))
+
+  next()
+})
+
 Vue.filter('truncate', (body, length) => {
   let ending = '...'
   if (body && body.length > length) {
