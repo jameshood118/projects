f980a9bc968c98ee87d91cc5defff23afc344eac
James Hood <jameshood118@gmail.com>
2020-08-16 17:38:55 -0700

    typos and updated to dynamic modal title

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -21,26 +21,36 @@
               <div class="buttons">
                 <button
                   class="button is-primary"
-                  @click="handleModalClick('Primary message', 'is-primary')"
+                  @click="
+                    handleModalClick('Primary', 'Primary message', 'is-primary')
+                  "
                 >
                   Open Main Modal
                 </button>
 
                 <button
                   class="button is-danger"
-                  @click="handleModalClick('DANGER WILL ROBINSON', 'is-danger')"
+                  @click="
+                    handleModalClick(
+                      'Danger',
+                      'DANGER WILL ROBINSON',
+                      'is-danger'
+                    )
+                  "
                 >
                   Open Danger Modal
                 </button>
                 <button
                   class="button is-warning"
-                  @click="handleModalClick('Warning Message', 'is-warning')"
+                  @click="
+                    handleModalClick('Warning', 'Warning Message', 'is-warning')
+                  "
                 >
                   Open Warning Modal
                 </button>
                 <button
                   class="button is-info"
-                  @click="handleModalClick('Info Message', 'is-info')"
+                  @click="handleModalClick('Info', 'Info Message', 'is-info')"
                 >
                   Open Info Modal
                 </button>
@@ -176,7 +186,7 @@
     </section>
 
     <dynamic-modal :open="modalOpen" @close="handleModalClose" v-if="modalOpen">
-      <template slot="modal-title">Dyanmic Modal</template>
+      <template slot="modal-title">{{ modalTitle }} Modal</template>
       <template slot="modal-body">
         <span :class="'tag is-round ' + modalTagClass">
           {{ modalMessage }}
@@ -185,7 +195,7 @@
       <template slot="modal-footer-buttons">
         <p class="control">
           <button class="button is-primary" @click="handleModalClose">
-            Save
+            Call to action
           </button>
         </p>
       </template>
@@ -201,6 +211,7 @@ export default {
       modalOpen: false,
       modalMessage: '',
       modalTagClass: 'is-primary',
+      modalTitle: '',
       emptyStateData: [],
       emptyStateLoading: true,
       isLoading: true,
@@ -220,13 +231,15 @@ export default {
         this.isLoading = action
       }
     },
-    handleModalClick(msg, tagClass) {
+    handleModalClick(title, msg, tagClass) {
       this.modalOpen = true
+      this.modalTitle = title
       this.modalMessage = msg
       this.modalTagClass = tagClass
     },
     handleModalClose() {
       this.modalOpen = false
+      this.modalTitle = ''
       this.modalMessage = ''
       this.modalTagClass = ''
     }
