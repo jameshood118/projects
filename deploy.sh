1f9bcd539c8e1318ce5567d75cec3b2128e0a1ca
James Hood <jameshood118@gmail.com>
2025-11-24 20:29:53 -0500

    updates

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 # build
+# Vite builds to the 'dist' folder by default
 npm run build
 
 # navigate into the build output directory
@@ -17,9 +18,11 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jameshood118/projects.git master:gh-pages
+# Note: Vite projects often use 'main' instead of 'master' for the default branch.
+# Verify your default branch name if the push fails.
+git push -f git@github.com:jameshood118/projects.git main:gh-pages
 
 cd -
